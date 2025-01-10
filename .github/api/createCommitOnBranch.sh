c381aaa52fdb1ba49f77682c570875a54348596d
Brad Slattman <slattman@gmail.com>
2025-01-10 16:06:27 -0500

    update

--- .github/api/createCommitOnBranch.sh
+++ .github/api/createCommitOnBranch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-GITHUB_HEAD_ID=$(git rev-parse HEAD) && GITHUB_FILE_CHANGES=$(rev <<< $(git diff --diff-filter="MT" --name-only $GITHUB_REF_NAME | awk '{system("contents=$(base64 -i "$1") && echo \"{ #path#: #"$1"#, #contents#: #$contents# },\"")}') | cut -c2- | rev | sed -e s/#/\\\"/g)
-git diff --diff-filter="MT" --name-only $GITHUB_REF_NAME && curl -H "Authorization: bearer $GH_TOKEN" -d @- https://api.github.com/graphql <<EOF
+GITHUB_HEAD_ID=$(git rev-parse HEAD) && GITHUB_FILE_CHANGES=$(rev <<< $(git diff --diff-filter=MT --name-only $GITHUB_REF_NAME | awk '{system("contents=$(base64 -i "$1") && echo \"{ #path#: #"$1"#, #contents#: #$contents# },\"")}') | cut -c2- | rev | sed -e s/#/\\\"/g)
+git diff --diff-filter=MT --name-only $GITHUB_REF_NAME && curl -H "Authorization: bearer $GH_TOKEN" -d @- https://api.github.com/graphql <<EOF
 {
   "query": "mutation(\$input:CreateCommitOnBranchInput!){createCommitOnBranch(input:\$input){commit{url}}}",
   "variables": {
