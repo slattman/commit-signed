f52096df063b1c3f31942aec395c2d42f52a6e66
Brad Slattman <slattman@gmail.com>
2025-01-10 22:29:36 -0500

    update

--- .github/api/createCommitOnBranch.sh
+++ .github/api/createCommitOnBranch.sh
@@ -4,7 +4,7 @@ gql=$(cat <<gql
     createCommitOnBranch(input: {
       branch: { repositoryNameWithOwner: "$GITHUB_REPOSITORY", branchName: "$GITHUB_REF_NAME" },
       message: { headline: "$GITHUB_JOB" },
-      fileChanges: { additions: [ $(rev <<< $(git diff --diff-filter=MT --name-only $GITHUB_REF_NAME | awk '{system("contents=$(base64 -i "$1") && echo \"{ path: \\\""$1"\\\", contents: \\\"$contents\\\" },\"")}') | cut -c2- | rev) ] },
+      fileChanges: { additions: [ $(rev <<< $(git diff --diff-filter=M --name-only $GITHUB_REF_NAME | awk '{system("contents=$(base64 -i "$1") && echo \"{ path: \\\""$1"\\\", contents: \\\"$contents\\\" },\"")}') | cut -c2- | rev) ] },
       expectedHeadOid: "$(git rev-parse HEAD)"
     }) { commit { url } }
   }
