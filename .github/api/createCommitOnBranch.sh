b08316958295c32c751b7ad9f560c59e0602f23d
Brad Slattman <slattman@gmail.com>
2025-01-10 20:22:10 -0500

    update

--- .github/api/createCommitOnBranch.sh
+++ .github/api/createCommitOnBranch.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
-GITHUB_HEAD_ID=$(git rev-parse HEAD)
-GITHUB_FILE_CHANGES=$( \
-  rev <<< $( \
-    git diff --diff-filter=MT --name-only $GITHUB_REF_NAME | \
-    awk '{system(" \
-      contents=$(base64 -i "$1") && \
-      echo \"{ #path#: #"$1"#, #contents#: #$contents# },\" \
-    ")}' \
-  ) | \
-  cut -c2- | \
-  rev | \
-  sed -e s/#/\\\"/g)
-curl -sH "Authorization: bearer $GH_TOKEN" -d @- https://api.github.com/graphql <<gql
-{
-  "query": "mutation(\$input:CreateCommitOnBranchInput!){createCommitOnBranch(input:\$input){commit{url}}}",
-  "variables": {
-    "input": {
-      "branch": {
-        "repositoryNameWithOwner": "$GITHUB_REPOSITORY",
-        "branchName": "$GITHUB_REF_NAME"
-      },
-      "message": {
-        "headline": "$GITHUB_JOB"
-      },
-      "fileChanges": {
-        "additions": [$GITHUB_FILE_CHANGES]
-      },
-      "expectedHeadOid": "$GITHUB_HEAD_ID"
-    }
+gql=$(cat <<gql
+  mutation () {
+    createCommitOnBranch(input: {
+      branch: { repositoryNameWithOwner: "$GITHUB_REPOSITORY", branchName: "$GITHUB_REF_NAME" },
+      message: { headline: "$GITHUB_JOB" },
+      fileChanges: { additions: [$(rev <<< $(git diff --diff-filter=MT --name-only $GITHUB_REF_NAME | awk '{system("contents=$(base64 -i "$1") && echo \"{ path: #"$1"#, contents: #$contents# },\"")}') | cut -c2- | rev | sed -e s/#/\\\"/g)] },
+      expectedHeadOid: "$(git rev-parse HEAD)"
+    }) { commit { url } }
   }
-}
-gql
+gql) && gh api graphql -f query="$gql"
